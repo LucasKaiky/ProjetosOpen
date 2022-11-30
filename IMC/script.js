6aa9b6f1afb71877cd752c31e1d5b46454ce3e17
LucasKaiky <lucaspessoapinho@gmail.com>
2022-11-29 21:04:13 -0300

    projetoNovo

---
+++ IMC/script.js
@@ -0,0 +1,88 @@
+// capturar evento de submit
+const form = document.querySelector('#form')
+
+//Tirar o atualizar com o event
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //pegar os imputs com o event
+    const inputpeso = event.target.querySelector('#peso')
+    const inputaltura = event.target.querySelector('#altura')
+
+    const peso = Number(inputpeso.value)
+    const altura = Number(inputaltura.value)
+
+
+    if (!peso) {
+        setResul('Peso Inválido!', false)
+        return
+    }
+
+    if (!altura){
+        setResul('Altura inválida!', false)
+        return
+    }
+
+    const imc = getImc(peso, altura)
+    const nivelImc = getNivel(imc)
+
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`
+
+    setResul(msg, true)
+})
+
+//Função para pegar o imc e o nível
+function getNivel (imc){
+    const nivel = ['Abaixo do peso' ,'Peso normal' ,'Sobrepeso' ,'Obesidade grau 1' ,
+    'Obesidade grau 2' ,'Obesidade grau 3']
+
+    if(imc >= 39.9){
+        return nivel[5]
+}
+
+    if(imc >= 34.9){
+        return nivel[4]
+}
+
+    if(imc >= 29.9){
+        return nivel[3]
+}
+
+    if(imc >= 24.9){
+        return nivel[2]
+}
+
+    if(imc >= 18.5){
+        return nivel[1]
+}
+
+    if(imc < 18.5){
+        return nivel[0]
+}
+}
+
+//função para calcular o imc
+function getImc(peso, altura){
+    const imc = peso / altura ** 2
+    return imc.toFixed(2)
+}
+
+//função para criar parágrafo
+function criaP(){
+    const p = document.createElement('p')
+    return p
+}
+
+//função para setar a mensagem e conferir a 
+function setResul(msg, isValid){
+    const resultado = document.querySelector('#resultado')
+    resultado.innerHTML = ''
+    
+    const p = criaP()
+    if (isValid) {
+        p.classList.add("pR")
+    } else {
+        p.classList.add('bad')
+    }
+    p.innerHTML= msg
+    resultado.appendChild(p)
+}
